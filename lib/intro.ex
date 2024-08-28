84dd34dc34c9aca4af09a5793cfdd87d6825bd08
Philipuden <philip.uden@elev.ga.ntig.se>
2024-08-28 15:35:51 +0200

    wla

--- lib/intro.ex
+++ lib/intro.ex
@@ -1,5 +1,36 @@
 defmodule Intro do
-  def hello do
-    :world
+  def smallest(num1, num2) when num1 < num2, do: num1
+  def smallest(_, num2), do: num2
+
+  def largest(num1, num2, num3) when num1 > num2 and num1 > num3, do: num1
+  def largest(_, num2, num3) when num2 > num3, do: num2
+  def largest(_, _, num3), do: num3
+
+  def smallest4(num1, num2, num3, num4) when num1 < num2 and num1 < num3 and num1 < num4, do: num1
+  def smallest4(_, num2, num3, num4) when num2 < num3 and num2 < num4, do: num2
+  def smallest4(_, _, num3, num4) when num3 < num4, do: num4
+  def smallest4(_, _, _, num4), do: num4
+
+  def ticketprice(age) when age < 18, do: 10
+  def ticketprice(age) when age > 64, do: 15
+  def ticketprice(_), do: 20
+
+  def nextnumber(num) do
+    num + 1
   end
+
+  def fact(1), do: 1
+  def fact(n), do: n*fact(n-1)
+
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n) when n > 1, do: fib(n-1) + fib(n-2)
+
+  def lse(list) when list == [], do: true
+  def lse(_) , do: false
+
+  def lsf(list), do: hd(list)
+
+  def lsl(list) when tl(list) != []  ,do: lsl(tl(list))
+  def lsl(list) ,do: hd(list)
 end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,99 @@
 defmodule IntroTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Intro.hello() == :world
-  end
+  # test "smallest of two when smallest is first" do
+  #   assert Intro.smallest(1,2) == 1
+  # end
+
+  # test "smallest of two when smallest is last" do
+  #   assert Intro.smallest(3,2) == 2
+  # end
+
+  # test "smallest of two of equal size" do
+  #   assert Intro.smallest(1337,1337) == 1337
+  # end
+
+  # test "largest of three when biggest is first" do
+  #   assert Intro.largest(3,2,1) == 3
+  # end
+
+  # test "largest of three when biggest is in the middle" do
+  #   assert Intro.largest(3,9,6) == 9
+  # end
+
+  # test "largest of three when biggest is last" do
+  #   assert Intro.largest(3,2,5) == 5
+  # end
+
+  # test "largest of three when all three is equal" do
+  #   assert Intro.largest(3,3,3) == 3
+  # end
+
+  # test "smallest of four when smallest is first" do
+  #   assert Intro.smallest4(1,2,3,4) == 1
+  # end
+
+  # test "smallest of four when smallest is last" do
+  #   assert Intro.smallest4(15,35,35,4) == 4
+  # end
+
+  # test "smallest of four when smallest is second" do
+  #   assert Intro.smallest4(12,2,3,4) == 2
+  # end
+
+  # test "ticket price for age under 18" do
+  #   assert Intro.ticketprice(17) == 10
+  # end
+
+  # test "ticket price for age over 64" do
+  #   assert Intro.ticketprice(67) == 15
+  # end
+
+  # test "ticket price for age beetween 17 and 65" do
+  #   assert Intro.ticketprice(55) == 20
+  # end
+
+  # test "next number" do
+  #   assert Intro.nextnumber(1) == 2
+  # end
+
+  # test "factorial for 7" do
+  #   assert Intro.factorial(-1) == 1
+  # end
+
+  # test "fibonnaci for 4" do
+  #   assert Intro.fib(4) == 3
+  # end
+
+  # test "fibonnaci for 6" do
+  #   assert Intro.fib(6) == 8
+  # end
+
+  # test "fibonnaci for 8" do
+  #   assert Intro.fib(8) == 21
+  # end
+
+  # test "list not empty" do
+  #   assert Intro.lse([:apple, 3.12]) == false
+  # end
+
+  # test "list empty" do
+  #   assert Intro.lse([]) == true
+  # end
+
+  # test "list first element" do
+  #   assert Intro.lsf([:apple, 3.12]) == :apple
+  # end
+
+  # test "list last element" do
+  #   assert Intro.lsf([:apple, 3.12]) == 3.12
+  # end
+
+  # test "list first element" do
+  #   assert Intro.lsl([:apple, "hej", 3.12]) == :apple
+  # end
+
+  # test "list last element" do
+  #   assert Intro.lsl([:apple, 3.12, "hej"]) == "hej"
+  # end
 end
